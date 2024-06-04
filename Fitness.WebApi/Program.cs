692394c0bac11e10be1bffd48a8d4d1f50fa8e64
Ratna Bhindi <ratna.bhindi@gmail.com>
2024-06-04 17:28:23 +1000

    Configures how the API controllers handle validation errors and input parsing errors, returning appropriate ProblemDetails objects with custom information

--- Fitness.WebApi/Program.cs
+++ Fitness.WebApi/Program.cs
@@ -1,5 +1,7 @@
 using Fitness.Application.Services.Implementation;
 using Fitness.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fitness.WebApi
 {
@@ -12,7 +14,64 @@ namespace Fitness.WebApi
             // Add services to the container.
             builder.Services.AddSingleton<IWorkoutService, WorkoutService>();
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers(options =>
+            {
+                options.RespectBrowserAcceptHeader = true;
+                options.ReturnHttpNotAcceptable = true;
+            }).ConfigureApiBehaviorOptions(setupAction =>
+            {
+                setupAction.InvalidModelStateResponseFactory = context =>
+                {
+                    // create a problem details object
+                    var problemDetailsFactory = context.HttpContext.RequestServices
+                        .GetRequiredService<ProblemDetailsFactory>();
+                    var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                            context.HttpContext,
+                            context.ModelState);
+
+                    // add additional info not added by default
+                    problemDetails.Detail = "See the errors field for details.";
+                    problemDetails.Instance = context.HttpContext.Request.Path;
+
+                    // find out which status code to use
+                    var actionExecutingContext =
+                          context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+                    // if there are modelstate errors & all keys were correctly
+                    // found/parsed we're dealing with validation errors
+                    //
+                    // if the context couldn't be cast to an ActionExecutingContext
+                    // because it's a ControllerContext, we're dealing with an issue 
+                    // that happened after the initial input was correctly parsed.  
+                    // This happens, for example, when manually validating an object inside
+                    // of a controller action.  That means that by then all keys
+                    // WERE correctly found and parsed.  In that case, we're
+                    // thus also dealing with a validation error.
+                    if (context.ModelState.ErrorCount > 0 &&
+                        (context is ControllerContext ||
+                         actionExecutingContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
+                    {
+                        problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
+                        problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                        problemDetails.Title = "One or more validation errors occurred.";
+
+                        return new UnprocessableEntityObjectResult(problemDetails)
+                        {
+                            ContentTypes = { "application/problem+json" }
+                        };
+                    }
+
+                    // if one of the keys wasn't correctly found / couldn't be parsed
+                    // we're dealing with null/unparsable input
+                    problemDetails.Status = StatusCodes.Status400BadRequest;
+                    problemDetails.Title = "One or more errors on input occurred.";
+                    return new BadRequestObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json" }
+                    };
+                };
+            }); ;
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
